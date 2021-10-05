8d6a6184b6d42b3d4e9068fa2668680e0acf6b9a
LucianoAlmeida <passos.luciano@outlook.com>
2021-10-05 00:25:10 -0300

    Making the size fixed accross all objects

--- Sources/strings/FixedBitArray.swift
+++ Sources/strings/FixedBitArray.swift
@@ -3,19 +3,20 @@ internal struct FixedBitArray {
   @usableFromInline
   typealias WordStorage = Array<UInt>
   
+  // 64 on 64-bits systems, 32 on 32-bits systems
+  @usableFromInline
+  internal static let _bitsPerWord = UInt.bitWidth
+  
   @usableFromInline
   internal var _words: WordStorage
-  
-  @usableFromInline
-  internal let _bitsPerWord = UInt.bitWidth // 64 on 64-bits systems, 32 on 32-bits systems
-  
+
   @usableFromInline
   internal let count: Int
   
   @inlinable
   @inline(__always)
   internal init(count: Int) {
-    let (div, remainder) = count.quotientAndRemainder(dividingBy: _bitsPerWord)
+    let (div, remainder) = count.quotientAndRemainder(dividingBy: FixedBitArray._bitsPerWord)
     self._words = WordStorage(repeating: 0, count: div &+ remainder.signum())
     self.count = count
   }
@@ -24,13 +25,13 @@ internal struct FixedBitArray {
   @inline(__always)
   internal subscript(_ idx: Int) -> Bool {
     get {
-      precondition(0..<count ~= idx, "Out of bounds index!")
-      let (pos, posIdx) = idx.quotientAndRemainder(dividingBy: _bitsPerWord)
+      assert(0..<count ~= idx, "Out of bounds index!")
+      let (pos, posIdx) = idx.quotientAndRemainder(dividingBy: FixedBitArray._bitsPerWord)
       return _words[pos] & (1 << posIdx) != 0
     }
     set {
-      precondition(0..<count ~= idx, "Out of bounds index!")
-      let (pos, posIdx) = idx.quotientAndRemainder(dividingBy: _bitsPerWord)
+      assert(0..<count ~= idx, "Out of bounds index!")
+      let (pos, posIdx) = idx.quotientAndRemainder(dividingBy: FixedBitArray._bitsPerWord)
       if newValue == true {
         _words[pos] |= (1 << posIdx)
       } else {
